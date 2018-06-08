487f473a6e6f8816cbb7936bc556ab0cfe65ced5
hsimao <e087754958@gmail.com>
2018-06-08 11:08:21 +0800

    修正自訂最大寬功能，修正滑動異常

--- js/js.js
+++ js/js.js
@@ -1,43 +1,43 @@
 $.fn.extend({
     _slider: function(def){
         // base 參數
-        let speed = 0
-        let currentValue = 0
         let index = 0
-        let current = null
-        let imgWidth = this.css('width') // 取得item寬
-        let items = this.find('.slider-item')
-        let timer = null
-        let timerAuto = null
-        let touchDiff = 0
-        let startX = null
+        let nextIndex = 0
+        let imgWidth = null  // 取得item寬
+        let items = this.find('.u-slider-item')
+        let timer = null     // 切換動換timmer
+        let timerAuto = null // 自動切換timer
+        let touchDiff = 0    // 拖曳距離
+        let startX = null    // 點擊當下x軸
         // dom
-        let box = this.find('.slider-box')
+        let box = this.find('.u-slider-box')
         let _this = this
+        let lastMove = null
+        let img = this.find('.u-slider-img')
 
 
         // 自訂參數
         var def = {
             color: def.color || '#8e44ad',
             duration: def.duration || 3000,
-            aspectRatio: def.aspectRatio || '9 / 16',
+            aspectRatio: def.aspectRatio || '56.25%',
             height: def.height || 'auto',
             maxWidth: def.maxWidth || '1140px',
             isShadow: def.isShadow || false
-
         }
 
 
-        // 計算 width, 設定比例
+        // 計算 width, 設定比例、初始自訂顏色
         const calcWidth = function(){
-            imgWidth = _this.css('width')
-            const link = box.find('.slider-link')
-            // _this.css('maxWidth', def.maxWidth)
+            _this.css('max-width', def.maxWidth)
+            _this.css('background-color', def.color)
+            const link = box.find('.u-slider-link')
             box.css('width', `${items.length*100}%`)
             box.css('height', def.height)
+            imgWidth = _this.css('width')
             for (let i=0; i<items.length; i++) {
                 items.eq(i).css('width', `${100/items.length}%`)
-                link.eq(i).css('padding-top', `calc(100% * (${def.aspectRatio}))`)
+                link.eq(i).css('padding-top', def.aspectRatio)
             }
         }
 
@@ -45,19 +45,18 @@ $.fn.extend({
         const createTool = function(){
             const colorPrev = `style='border-color: transparent ${def.color} transparent transparent'`
             const colorNext = `style='border-color: transparent transparent transparent ${def.color}'`
-            const tool = $('<div class="slider-tool"></div>')
-            const prev = $('<div class="slider-prev"><span class="slider-prev-bg"></span><span class="slider-prev-icon"'+colorPrev+'></span></div>')
-            const next = $('<div class="slider-next"><span class="slider-next-bg"></span><span class="slider-next-icon"'+colorNext+'></span></div>')
+            const tool = $('<div class="u-slider-tool"></div>')
+            const prev = $('<div class="u-slider-prev"><span class="u-slider-prev-bg"></span><span class="u-slider-prev-icon"'+colorPrev+'></span></div>')
+            const next = $('<div class="u-slider-next"><span class="u-slider-next-bg"></span><span class="u-slider-next-icon"'+colorNext+'></span></div>')
             $(tool).append(prev)
             $(tool).append(next)
 
             // create btn
-            const btn = $('<div class="slider-btn"></div>')
+            const btn = $('<div class="u-slider-btn"></div>')
             for (let i=0; i<items.length; i++) {
                 const color = `style="background-color: ${def.color}"`
-                const item = $('<div class="btn"><span class="btn-bg" '+color+'></span></div>')
+                const item = $('<div class="u-btn"><span class="u-btn-bg" '+color+'></span></div>')
                 item.attr('data-item', i)
-                item.click(() => updateIndex(i)) // bind event
                 btn.append($(item))
             }
             tool.append(btn)
@@ -67,8 +66,8 @@ $.fn.extend({
 
         const renderActive = function(){
             if (def.isShadow) _this.addClass('shadow')
-            $('.slider-tool .btn-bg').removeClass('active')
-            $('.slider-tool .btn-bg').eq(index).addClass('active')
+            _this.find('.u-slider-tool .u-btn-bg').removeClass('active')
+            _this.find('.u-slider-tool .u-btn-bg').eq(index).addClass('active')
         }
 
 
@@ -91,7 +90,6 @@ $.fn.extend({
             clearInterval(timer)
             let speed = 0
             let currentValue = 0
-
             timer = setInterval(() => {
                 // 換算成百分比
                 currentValue = Math.abs(Math.floor((parseInt(el.css(attr)) / parseInt(imgWidth))*100))
@@ -111,8 +109,37 @@ $.fn.extend({
             },30)
         }
 
+        // 轉場期間添加class
+        const addClass = () => {
+            img.removeClass('before')
+            img.removeClass('active')
+            img.removeClass('after')
+            img.eq(nextIndex+1).addClass('before')
+            img.eq(nextIndex-1).addClass('before')
+            img.eq(index).addClass('active')
+            img.eq(nextIndex-1).addClass('after')
+        }
 
         const updateIndex = function(target){
+            // 取出nextIndex
+            if (target == 'prev') {
+                if (index == 0) {
+                    nextIndex = items.length-1
+                } else {
+                    nextIndex = index-1
+                }
+            }
+
+            if (target == 'next') {
+                if (index == items.length-1) {
+                    nextIndex = 0
+                } else {
+                    nextIndex = index+1
+                }
+            }
+
+            // addClass()
+
             if (target === 'prev') {
                 index--
                 if (index < 0) {
@@ -151,11 +178,13 @@ $.fn.extend({
 
         const touch = function() {
             _this.on('mousedown touchstart', function(e){
+                lastMove = e //將事件儲存
                 e.preventDefault()
                 clearInterval(timerAuto)
                 startX = e.pageX || e.originalEvent.touches[0].pageX
                 _this.on('mousemove touchmove', function(e){
-                    const x = e.pageX || e.originalEvent.touches[0].pageX;
+                    lastMove = e //將事件儲存
+                    const x = lastMove.pageX || lastMove.originalEvent.touches[0].pageX;
                     touchDiff = Math.floor((startX - x) / parseInt(imgWidth) * 70);
                     if ((!index && touchDiff < 0) || (index === index-1 && touchDiff > 0)) touchDiff /= 2;
                     box.css('marginLeft', `calc(-${index*100}% - ${touchDiff}px)`)
@@ -166,15 +195,17 @@ $.fn.extend({
 
         const touchout = function() {
             _this.on('mouseup touchend', function(e){
+                // touchen沒有事件座標,必須向上取得touchmove或touchstart資訊
+                let lastPageX = lastMove.pageX || lastMove.originalEvent.touches[0].pageX
                 e.preventDefault()
                 $(_this).off('mousemove touchmove') //取消touch事件
-
-                if (e.pageX == startX ) {
+                if (lastPageX == startX ) {
                     //如果沒有移動,當成點擊切換
-                    if (e.target.classList.contains('slider-next')) updateIndex('next')
-                    if (e.target.classList.contains('slider-next-bg')) updateIndex('next')
-                    if (e.target.classList.contains('slider-prev')) updateIndex('prev')
-                    if (e.target.classList.contains('slider-prev-bg')) updateIndex('prev')
+                    if (e.target.classList.contains('u-btn')) updateIndex($(e.target).attr('data-item'))
+                    if (e.target.classList.contains('u-slider-next')) updateIndex('next')
+                    if (e.target.classList.contains('u-slider-next-bg')) updateIndex('next')
+                    if (e.target.classList.contains('u-slider-prev')) updateIndex('prev')
+                    if (e.target.classList.contains('u-slider-prev-bg')) updateIndex('prev')
 
                 } else if (touchDiff > 0 ) {
                     //往左拖曳下一頁
@@ -194,6 +225,7 @@ $.fn.extend({
             autoChange()
             touch()
             touchout()
+            // addClass()
             $(window).resize(()=>{
                 calcWidth()
             })
